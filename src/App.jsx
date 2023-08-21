7cda93d9687dc61d55adcf3c9cfec68320bad91c
William Alves <williamcostacardoso94@gmail.com>
2023-08-20 22:30:35 -0300

    inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+
+const App = () => {
+  return (
+  <BrowserRouter>      
+    <AppRoutes />
+  </BrowserRouter>
+  );
+}
+
+export default App;
+
+
